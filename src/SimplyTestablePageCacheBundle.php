caadb82227e0aa728206b38109f2dd51a5da4b6b
Jon Cram <jon@webignition.net>
2018-08-24 17:24:57 +0100

    Fix bundle name

--- src/SimplyTestablePageCacheBundle.php
+++ src/SimplyTestablePageCacheBundle.php
@@ -4,6 +4,6 @@ namespace SimplyTestable\PageCacheBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AcmeTestBundle extends Bundle
+class SimplyTestablePageCacheBundle extends Bundle
 {
 }
