40077c7bf4c827c47758b9f70d1ea5e3671003a8
Jon Cram <jon@webignition.net>
2018-08-26 10:10:54 +0100

    Define compiler pass as anonymous class

--- Tests/Functional/AppKernel.php
+++ Tests/Functional/AppKernel.php
@@ -6,6 +6,8 @@ use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use SimplyTestable\PageCacheBundle\SimplyTestablePageCacheBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
@@ -31,7 +33,26 @@ class AppKernel extends Kernel
     protected function buildContainer()
     {
         $container = parent::buildContainer();
-        $container->addCompilerPass(new FooCompilerPass());
+        $container->addCompilerPass(new class implements CompilerPassInterface {
+            public function process(ContainerBuilder $container)
+            {
+                foreach ($container->getDefinitions() as $id => $definition) {
+                    if (strpos($id, 'simplytestable_pagecache') === false) {
+                        continue;
+                    }
+
+                    $definition->setPublic(true);
+                }
+
+                foreach ($container->getAliases() as $id => $alias) {
+                    if (strpos($id, 'SimplyTestable\\PageCacheBundle\\') === false) {
+                        continue;
+                    }
+
+                    $alias->setPublic(true);
+                }
+            }
+        });
 
         return $container;
     }
