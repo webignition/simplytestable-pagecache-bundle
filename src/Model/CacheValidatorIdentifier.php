3acc040e3fb1e4cbc44d252c809904e7ed273ee3
Jon Cram <jon@webignition.net>
2018-08-29 16:56:56 +0100

    Add support in CacheValidatorIdentifier::__toString() for wider non-scalar types

--- src/Model/CacheValidatorIdentifier.php
+++ src/Model/CacheValidatorIdentifier.php
@@ -4,6 +4,13 @@ namespace SimplyTestable\PageCacheBundle\Model;
 
 class CacheValidatorIdentifier
 {
+    const BOOL_TRUE_STRING = 'true';
+    const BOOL_FALSE_STRING = 'false';
+    const NULL_STRING = 'null';
+
+    const EXCEPTION_CODE_OBJECT_CANNOT_BE_CONVERTED_TO_STRING = 1;
+    const EXCEPTION_CODE_VALUE_CANNOT_BE_CONVERTED_TO_STRING = 2;
+
     /**
      * @var array
      */
@@ -29,13 +36,48 @@ class CacheValidatorIdentifier
     {
         $keyValuePairs = array();
         foreach ($this->parameters as $key => $value) {
-            if (is_bool($value)) {
-                $value = $value ? 'true' : 'false';
-            }
-
-            $keyValuePairs[] = $key . ':' . $value;
+            $keyValuePairs[] = $key . ':' . $this->convertValueToString($value, $key);
         }
 
         return md5(implode('+', $keyValuePairs));
+    }
+
+    private function convertValueToString($value, $key): string
+    {
+        if (is_bool($value)) {
+            return $value ? self::BOOL_TRUE_STRING : self::BOOL_FALSE_STRING;
+        }
+
+        if (is_null($value)) {
+            return self::NULL_STRING;
+        }
+
+        if (is_scalar($value)) {
+            return (string)$value;
+        }
+
+        if (is_array($value)) {
+            return json_encode($value);
+        }
+
+        if (is_object($value)) {
+            if (method_exists($value, '__toString')) {
+                return (string)$value;
+            }
+
+            if ($value instanceof \JsonSerializable) {
+                return json_encode($value);
+            }
+
+            throw new \UnexpectedValueException(
+                sprintf('Object of type %s cannot be converted to a string', get_class($value)),
+                self::EXCEPTION_CODE_OBJECT_CANNOT_BE_CONVERTED_TO_STRING
+            );
+        }
+
+        throw new \UnexpectedValueException(
+            sprintf('Value with key "%s" cannot be converted to a string', $key),
+            self::EXCEPTION_CODE_VALUE_CANNOT_BE_CONVERTED_TO_STRING
+        );
     }
 }
